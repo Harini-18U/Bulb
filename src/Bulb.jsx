5faf4d51d989eb311d2371c07923d03f69c5c78c
Harini_U <harini.innovator@gmail.com>
2024-04-29 19:49:38 +0530

    first git upload

---
+++ src/Bulb.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ToggleBulbOnAndOff = () => {
+    const [light, setLight] = React.useState(false);
+    const toggleDisplay = () => {
+        setLight(!light);
+    };
+    return (
+        <>
+        <button onClick={toggleDisplay}>Light {light ? 'on':'off'}</button>
+        {light ? (
+            <img src="https://media.geeksforgeeks.org/wp-content/uploads/ONbulb.jpg" alt="on"/>
+        ) : (
+            <img src="https://media.geeksforgeeks.org/wp-content/uploads/OFFbulb.jpg" alt="off"/>
+        )}
+        </>
+    );
+};
+export default ToggleBulbOnAndOff;
